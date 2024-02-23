2c61fb5066b1ffd1b97634cc3eb0497f08d32804
ymahendraa <ymahendra721@gmail.com>
2024-02-23 16:32:09 +0800

    feat (config): configuration.ts

---
+++ configuration.ts
@@ -0,0 +1,9 @@
+const configuration = {
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+  },
+};
+
+export { configuration };
