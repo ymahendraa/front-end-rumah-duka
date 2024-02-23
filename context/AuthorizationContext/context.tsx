ca57d356b7383fa2b244095cb2dd1720f27df049
ymahendraa <ymahendra721@gmail.com>
2024-02-23 16:50:32 +0800

    feat (context): AuthorizationContext =>  initializing of permission data

--- context/AuthorizationContext/context.tsx
+++ context/AuthorizationContext/context.tsx
@@ -1,32 +1,39 @@
 'use client'
-import React, { createContext, useState } from 'react';
+import useFetcher from '@/hooks/useFetcher';
+import { useSession } from 'next-auth/react';
+import React, { createContext } from 'react';
+import useSWR from 'swr';
 
 // Define the initial state as undefined or null
 const initialState = {
     data: undefined,
-    setState: (newState: any) => { },
 };
 
 // Create the context
 const AuthorizationContext = createContext(initialState);
 
 // Create a provider component that wraps your app
-const AuthorizationProvider = ({ children }: { children: React.ReactNode }) => {
-    // Use the useState hook to manage the state
-    const [data, setData] = useState<any>(initialState.data);
+const AuthorizationProvider = React.memo(({ children }: { children: React.ReactNode }) => {
+    // get session data
+    const { data: session } = useSession()
 
-    // Define the setter function
-    const setState = (newState: any) => {
-        setData(newState);
-    };
+    // fetcher function
+    const fetcher = useFetcher(session)
+
+    // useSWR hook to fetch data
+    const { data } = useSWR(session ? 'http://localhost:3000/api/redis' : null,
+        fetcher, {
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false
+    })
 
     // Provide the state and setter function to the children components
     return (
-        <AuthorizationContext.Provider value={{ data, setState }}>
+        <AuthorizationContext.Provider value={data}>
             {children}
         </AuthorizationContext.Provider>
     );
-};
+});
 
 // Export the context and provider
 export { AuthorizationContext, AuthorizationProvider };
