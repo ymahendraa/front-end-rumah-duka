3692d01fc60e5654c1bab86b8ce923b6a2d74d90
ymahendraa <ymahendra721@gmail.com>
2024-03-12 09:22:03 +0800

    feat (component): dashboard/ => new ruangan feature boilerplate

---
+++ app/(protected)/dashboard/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Home: React.FC = () => {
+
+    return (
+        <div className="h-full flex flex-col items-center justify-center bg-base">
+            <main className="mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-center py-12">
+                    <Image src="/logo.png" alt="Logo" width={72} height={72} />
+                </div>
+                <h1 className="text-xl md:text-3xl font-bold text-center text-gray-900">
+                    Welcome to Random Management System
+                </h1>
+                <p className="mt-4 text-sm md:text-lg text-center text-gray-600">
+                    This is a base application build with NextJS for Random Management System
+                </p>
+            </main>
+        </div>
+    )
+}
+
+export default Home
