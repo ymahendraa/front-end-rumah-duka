9718513dc8d7de882803ffa76919c212fd68da86
ymahendraa <ymahendra721@gmail.com>
2024-03-12 09:08:49 +0800

    feat (component): ruangan/ => new ruangan feature boilerplate

---
+++ app/(protected)/ruangan/(features)/(pages)/tambah-data/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Create from '../../components/Create'
+
+const CreatePage = () => {
+    return (
+        <main>
+            <Create />
+        </main>
+    )
+}
+
+export default CreatePage
