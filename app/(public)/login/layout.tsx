60cbb5d79284aad2e03126da36ebf780acdc3790
ymahendraa <ymahendra721@gmail.com>
2024-03-12 09:01:18 +0800

    style (component): unauthorized style (component): login/layout.tsx

--- app/(public)/login/layout.tsx
+++ app/(public)/login/layout.tsx
@@ -6,7 +6,7 @@ const LoginLayout: React.FC<LoginLayoutProps> = ({ children }) => {
   return (
     <div className="w-full">
       <div>{children}</div>
-      <div className="login-footer bg-white text-slate-800 text-sm md:text-md pl-2">
+      <div className="login-footer bg-base text-slate-800 text-sm md:text-md pl-2">
         <p>Copyright &copy; 2023 CICT KALLA</p>
       </div>
     </div>
--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -18,7 +18,7 @@ const UnauthorizedPage: React.FC = () => {
         <Section className="flex flex-col items-center justify-center w-full h-screen bg-gray-100">
             <Image src="./401.svg" alt="unauthorized" width={500} height={500} />
             <p className="text-lg text-gray-600 mb-8">You do not have permission to access this page.</p>
-            <Button className="px-4 py-1 bg-secondary text-white text-sm rounded-lg hover:bg-secondaryDark" onClick={handleBack}>Go back</Button>
+            <Button className="px-4 py-1 bg-secondary text-white text-sm rounded-lg hover:bg-secondary-dark" onClick={handleBack}>Go back</Button>
         </Section>
     );
 };
