74873463f615ed329f3f3764a1c6f7e17b059402
ymahendraa <ymahendra721@gmail.com>
2024-05-18 23:22:35 +0800

    feat (api): profile, reset-password

---
+++ app/api/authorization/profile/[id]/route.ts
@@ -0,0 +1,120 @@
+import { NextRequest, NextResponse } from "next/server";
+// import jwt from "jsonwebtoken";
+
+export async function GET(
+  req: NextRequest,
+  params: { params: { id: string } }
+) {
+  const { id } = params.params;
+  // get token from headers
+  const token = req.headers.get("authorization")?.split(" ")[1] || "";
+
+  // if token does not exist, return an error
+  if (!token) {
+    return NextResponse.json({ error: "Token doesn't exist" }, { status: 401 });
+  }
+  // if token exists, verify it
+  else {
+    // fetch data from json server
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_REAL_URL}/admin/${id}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      }
+      return NextResponse.json({ error: "Error" }, { status: response.status });
+    }
+
+    const { data } = await response.json();
+    // return NextResponse.json(data);
+    return NextResponse.json(data);
+  }
+}
+
+export async function PATCH(
+  req: NextRequest,
+  params: { params: { id: string } }
+) {
+  const { id } = params.params;
+  // get token from headers
+  const token = req.headers.get("authorization")?.split(" ")[1] || "";
+  // get body from request
+  const body = await req.json();
+
+  // if token does not exist, return an error
+  if (!token) {
+    return NextResponse.json({ error: "Token doesn't exist" }, { status: 401 });
+  }
+  // if token exists, verify it
+  else {
+    // delete role in body
+    delete body.role;
+    // fetch data from json server
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_REAL_URL}/profile`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(body),
+      }
+    );
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      }
+      return NextResponse.json({ error: "Error" }, { status: response.status });
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  params: { params: { id: string } }
+) {
+  const { id } = params.params;
+  // get token from headers
+  const token = req.headers.get("authorization")?.split(" ")[1] || "";
+
+  // if token does not exist, return an error
+  if (!token) {
+    return NextResponse.json({ error: "Token doesn't exist" }, { status: 401 });
+  }
+  // if token exists, verify it
+  else {
+    // fetch data from json server
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_REAL_URL}/admin/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      }
+      return NextResponse.json({ error: "Error" }, { status: response.status });
+    }
+
+    const data = await response.text();
+    return NextResponse.json(data);
+  }
+}
