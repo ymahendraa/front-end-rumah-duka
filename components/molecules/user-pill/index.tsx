1848a9088ba6237c31ff4295fc49cad17cea9fce
ymahendraa <ymahendra721@gmail.com>
2024-05-18 15:33:32 +0800

    feat (component): Userpill => add username and role dynamically

--- components/molecules/user-pill/index.tsx
+++ components/molecules/user-pill/index.tsx
@@ -5,19 +5,22 @@ import Section from '@/components/atoms/section'
 import { UserCircleIcon } from '@heroicons/react/24/outline'
 
 type UserPillProps = {
-    minimized?: boolean
+    minimized?: boolean,
+    username?: string
+    role?: string
 }
 
 const UserPill: React.FC<UserPillProps> = ({
-    minimized
+    minimized,
+    ...props
 }) => {
     return (
-        <Section className='w-full flex justify-start items-center px-2 gap-x-3'>
+        <Section className='max-w-full flex justify-start items-center px-2 gap-x-3'>
             <Icon icon={<UserCircleIcon className='h-10 w-10 text-white' />} />
             {!minimized &&
                 <Section className='flex flex-col'>
-                    <p className='text-white text-sm'>Admin</p>
-                    <p className='text-gray-600 text-xs'>Admin</p>
+                    <p className='text-white text-sm' >{props.username ?? 'User'}</p>
+                    <p className='text-gray-600 text-xs'>{props.role ?? ''}</p>
                 </Section>
             }
         </Section>
